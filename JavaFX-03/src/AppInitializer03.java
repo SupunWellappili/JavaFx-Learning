4d8a2bee5fcc90c9b6424ed5ed015e3795db378f
SupunWellappili <supunchandana1814@gmail.com>
2024-05-09 00:19:50 +0530

    primaryStage.show()

--- JavaFX-03/src/AppInitializer03.java
+++ JavaFX-03/src/AppInitializer03.java
@@ -1,5 +1,9 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppInitializer03 extends Application {
 
@@ -8,7 +12,8 @@ public class AppInitializer03 extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("MainFrom03.fxml"))));
+        primaryStage.show();
     }
 }
