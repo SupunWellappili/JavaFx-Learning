728d07e0c91ece254d2ddb85a8d2b3121bf65403
SupunWellappili <supunchandana1814@gmail.com>
2024-05-09 00:37:07 +0530

    Create interface

--- JavaFX-03/src/MainFrom03Controller03.java
+++ JavaFX-03/src/MainFrom03Controller03.java
@@ -1,2 +1,15 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+
 public class MainFrom03Controller03 {
+    public CheckBox cbxJava;
+    public CheckBox cbxC;
+    public CheckBox cbxKotlin;
+    public CheckBox cbxRuby;
+    public ListView<String> languageList;
+
+    public void btnSubmitOnAction(ActionEvent actionEvent) {
+
+    }
 }
