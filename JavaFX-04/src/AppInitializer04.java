d69c4b5ff1b69c33c4324d73628f27765c51ab0c
SupunWellappili <supunchandana1814@gmail.com>
2024-05-17 12:52:08 +0530

    Create Login Interface

--- JavaFX-04/src/AppInitializer04.java
+++ JavaFX-04/src/AppInitializer04.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class AppInitializer extends Application {
+public class AppInitializer04 extends Application {
 
     public static void main(String[] args) {
         launch(args);
