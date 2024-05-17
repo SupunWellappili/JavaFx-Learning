b5dac52e29c815a362093c04dcb4888df0bab206
SupunWellappili <supunchandana1814@gmail.com>
2024-05-18 00:01:59 +0530

    include subWindow code

--- JavaFx-06/src/MainFormController06.java
+++ JavaFx-06/src/MainFormController06.java
@@ -2,12 +2,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 
 public class MainFormController06 {
+
+    public TextField txtInput;
 
     public void nextWindowOnAction(ActionEvent actionEvent) throws IOException {
         URL resource = getClass().getResource("subWindowFormController.fxml");
--- JavaFx-06/src/SubWindowForm.java
+++ JavaFx-06/src/SubWindowForm.java
@@ -1,2 +1,11 @@
+import javafx.scene.control.TextField;
+
 public class SubWindowForm {
+    public TextField txtImportedText;
+
+    public void setValue(String value){
+        txtImportedText.setText(value);
+    }
 }
+
+
