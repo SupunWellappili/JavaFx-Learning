d2cc08e95fffde32346d2ce3886f2b2c059762e9
SupunWellappili <supunchandana1814@gmail.com>
2024-05-17 23:39:59 +0530

    Navigation (Opening New Windows)

---
+++ JavaFx-06/src/MainFormController06.java
@@ -0,0 +1,21 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainFormController06 {
+
+    public void nextWindowOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("subWindowFormController.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Scene scene = new Scene(load);
+        Stage subStage = new Stage();
+        subStage.setScene(scene);
+        subStage.show();
+
+    }
+}
