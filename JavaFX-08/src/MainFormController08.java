c3d1262c231042972d47e72f33b08b7c5daab72c
SupunWellappili <supunchandana1814@gmail.com>
2024-05-22 08:23:44 +0530

    Create Form & Controller

---
+++ JavaFX-08/src/MainFormController08.java
@@ -0,0 +1,20 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
+public class MainFormController08 {
+    public AnchorPane mainFormContext;
+
+    public void openWindowOneInAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) mainFormContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("WindowsOneForm.fxml"))));
+    }
+
+    public void openWindowTwoInAction(ActionEvent actionEvent) {
+    }
+}
