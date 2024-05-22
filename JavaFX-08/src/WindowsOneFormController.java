f8a486c26046e6a56a5789e553a11c6029628f62
SupunWellappili <supunchandana1814@gmail.com>
2024-05-22 09:15:26 +0530

    Code

--- JavaFX-08/src/WindowsOneFormController.java
+++ JavaFX-08/src/WindowsOneFormController.java
@@ -1,3 +1,20 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class WindowsOneFormController {
 
+    public AnchorPane windowOneContext;
+
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) windowOneContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("MainForm08.fxml"))));
+
+    }
 }
--- JavaFX-08/src/WindowsTwoFormController.java
+++ JavaFX-08/src/WindowsTwoFormController.java
@@ -1,3 +1,18 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class WindowsTwoFormController {
 
+    public AnchorPane windowTwoContext;
+
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) windowTwoContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("MainForm08.fxml"))));
+
+    }
 }
